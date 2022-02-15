a3a0fe95b6daa5591f73d5a7e8322ed707f76eba
adok0001 <73196282+adok0001@users.noreply.github.com>
2022-02-15 01:02:19 -0800

    Incoming merge from trial branch

--- App.js
+++ App.js
@@ -1,25 +1,42 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+<<<<<<< Updated upstream
 import { Button, Image, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+=======
+import { Button, Dimensions, Image, Keyboard, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+>>>>>>> Stashed changes
 import * as ImagePicker from 'expo-image-picker';
 import * as Sharing from 'expo-sharing';
 import { NavigationContainer, TabActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { BottomTabBar, createBottomTabNavigator, useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 //import {createDrawerNavigator} from '@react-navigation/drawer';
 import Ionicons from '@expo/vector-icons/Ionicons';
+<<<<<<< Updated upstream
 
 const Tab = createBottomTabNavigator();
+=======
+import { BlurView } from 'expo-blur';
+import { getHeaderTitle } from '@react-navigation/elements';
+import { backgroundColor, textDecorationColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { TouchableWithoutFeedback } from 'react-native-web';
+
+import { data } from './Data';
+
+const Tab = createBottomTabNavigator();
+const { width } = Dimensions.get('screen');
+>>>>>>> Stashed changes
+
+const ITEM_WIDTH = width * 0.9;
+const ITEM_HEIGHT = ITEM_WIDTH * 0.9;
 
 function HomeScreen({ navigation, route }) {
-  React.useEffect(() => {
-    if (route.params?.post) {
-
-    }
-  }, [route.params?.post]);
+  const tabBarheight = useBottomTabBarHeight();
 
   return (
     <SafeAreaView style={styles.homeContainer}>
+<<<<<<< Updated upstream
     <ScrollView style={{marginHorizontal:5}}
     // style={styles.homeContainer}>
     >
@@ -42,6 +59,34 @@ function HomeScreen({ navigation, route }) {
 
       <Text style={styles.instructions}>Post: {route.params?.post}</Text>
     </ScrollView>
+=======
+      {/* <View style={{ flex: 1 }}> */}
+
+      {/* Scrollable container */}
+        <ScrollView
+          indicatorStyle="white"
+          contentContainerStyle={[
+            { paddingBottom: tabBarheight, alignItems: 'center' },
+          ]}
+        >
+          {data.map((item) => (
+            <View key={item.id} style={{ marginBottom: 14 }}>
+              <Image
+                style={styles.image}
+                source={{ uri: item.video_url }}
+                resizeMode="cover"
+              />
+              <Text style={styles.instructions}>{item.homeTeam} vs {item.awayTeam}</Text>
+              <Text style={styles.instructions}>Comp: {item.matchType} Time: {item.matchTime}</Text>
+            </View>
+          ))}
+          <Button title="Upload Screen" onPress={() => navigation.navigate("CreatePost")} />
+          <Text style={styles.instructions}>Post: {route.params?.post}</Text>
+          <Image source={{ uri: route.params?.media }} style={styles.image} />
+
+        </ScrollView>
+      {/* </View> */}
+>>>>>>> Stashed changes
     </SafeAreaView>
   )
 }
@@ -53,6 +98,7 @@ function CreatePostScreen({ navigation, route }) {
     navigation.setOptions({
       headerRight: () => (<Button onPress={() => navigation.navigate("Upload")} title="Upload" color='white' />)
     })
+<<<<<<< Updated upstream
   })
   
   return (
@@ -62,11 +108,37 @@ function CreatePostScreen({ navigation, route }) {
       <Button title="Post with Video" onPress={() => navigation.navigate({ name: "Upload", params: { post: postText }, merge: true, })} />
       <Button title='Titular update' onPress={() => navigation.setOptions({ headerTitle: "New Post" })} />
     </View>
+=======
+  });
+
+  useFocusEffect(
+    React.useCallback(() => {
+      return () => {
+        setPostText('');
+      }
+    }, [])
+  );
+
+
+  return (
+    <SafeAreaView style={styles.container} >
+      <TextInput placeholder="What's on your mind?" style={styles.post} value={postText} onChangeText={setPostText}
+        keyboardAppearance='dark' placeholderTextColor="white" returnKeyType='go'
+      />
+      <Button title="Post Text" onPress={() => navigation.navigate({ name: "Home", params: { post: postText }, merge: true, })} />
+      <Button title="Post with Video" onPress={() => navigation.navigate({ name: "Upload", params: { post: postText }, merge: true, })} />
+      <Button title='Titular update' onPress={() => navigation.setOptions({ headerTitle: "New Post" })} />
+
+    </SafeAreaView>
+
+
+>>>>>>> Stashed changes
   );
 
 }
 
-function BroadcastScreen({ navigation }) {
+function BroadcastScreen({ navigation, route }) {
+  const { post } = route.params;
   let [selectedImage, setSelectedImage] = React.useState(null);
 
   let openImagePickerAsync = async () => {
@@ -98,9 +170,8 @@ function BroadcastScreen({ navigation }) {
     return (
       <View style={styles.container}>
         <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
-        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Share this photo</Text>
-        </TouchableOpacity>
+        <Button title="Share Photo" onPress={() => navigation.navigate("Home", { media: selectedImage.localUri, post: post, })} style={styles.button} />
+        <Button title="Choose another Photo" onPress={() => setSelectedImage(null)} style={styles.button} />
       </View>
     );
   }
@@ -146,15 +217,31 @@ function LogoTitle() {
 
 const BroadcastStack = createNativeStackNavigator();
 
+<<<<<<< Updated upstream
 function UploadStackScreen(){
   return(
     <BroadcastStack.Navigator>
       <BroadcastStack.Screen name="Create Post" component={CreatePostScreen} />
       <BroadcastStack.Screen name="Upload" component={BroadcastScreen} />
+=======
+function UploadStackScreen() {
+
+  return (
+    <BroadcastStack.Navigator>
+      <BroadcastStack.Screen name="Create Post" component={CreatePostScreen} options={{ headerShown: false }} />
+      <BroadcastStack.Screen name="Upload" component={BroadcastScreen}
+        options={{
+          headerTitle: (props) => <LogoTitle {...props} />,
+          marginTop: 100,
+        }}
+
+      />
+>>>>>>> Stashed changes
     </BroadcastStack.Navigator>
   )
 }
 
+<<<<<<< Updated upstream
 export default function App(props) {
 
   return (
@@ -189,6 +276,89 @@ export default function App(props) {
         }} />
       </Tab.Navigator>
     </NavigationContainer>
+=======
+header: ({ navigation, route, options }) => {
+  const title = getHeaderTitle(options, route.name);
+  if (title === 'Home') {
+
+  }
+  return <MyHeader title={title} style={options.headerStyle} />;
+};
+
+const CustomTabBar = (props) => {
+  return (
+    <BlurView
+      style={{ position: 'absolute', bottom: 0, left: 0, right: 0, }}
+      tint="dark"
+      intensity={100}
+      blurRadius={50}
+      overlayColor="transparent"
+    >
+      <BottomTabBar {...props} />
+    </BlurView>
+  );
+
+};
+
+export default function App(props) {
+
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === 'Home') {
+                iconName = focused ? 'home'
+                  : 'home-outline';
+              } else if (route.name === 'Upload') {
+                iconName = 'cloud-upload';
+              } else if (route.name === 'CreatePost') {
+                iconName = "create";
+              } else if (route.name === 'GreenScreen') {
+                iconName = focused ? "color-palette" : "color-palette-outline";
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            //tabBarActiveTintColor: 'tomato',
+            tabBarStyle: {
+              position: 'absolute',
+              borderTopColor: 'black',
+              backgroundColor: 'black',
+              elevation: 0,
+            },
+            tabBarBackground: () => (<BlurView tint="dark" intensity={45} style={StyleSheet.absoluteFill} />),
+            tabBarBadgeStyle: { backgroundColor: 'blue' },
+          })}
+        //tabBar={(props) => <CustomTabBar {...props} />}
+
+        //mode="modal" initialRouteName='Home' screenOptions={{ headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white', headerTitleStyle: { fontWeight: 'bold' }, }} >
+        >
+          <Tab.Screen name="Home" component={HomeScreen}
+            options={{
+              headerTitle: "Match Day", headerStyle: {},
+              //headerTintColor:"white", 
+              headerTitleStyle: { fontWeight: 'bold', fontSize: 25, },
+              //, headerTransparent:true
+              // headerBackground: ()=>
+              // <Image style={{width: 500, height: 100,}} 
+              // source={require('./assets/thomas-serer-r-xKieMqL34-unsplash.jpg')} />,
+
+
+            }} />
+          {/* <Tab.Screen name="Upload" component={BroadcastScreen} options={{ headerTitle: (props) => <LogoTitle {...props} /> }} /> */}
+          <Tab.Screen name="GreenScreen" component={GreenScreen} initialParams={{ username: "tammyado" }} options={({ route }) => ({ title: route.params.name, tabBarBadge: 3 })} />
+          <Tab.Screen name="CreatePost" component={UploadStackScreen} options={{
+            headerShown: false,
+            tabBarLabel: 'Create Post',
+            //headerRight: () => (<Button onPress={() => alert("Upload")} title="Upload" color='white'/>), 
+          }} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+>>>>>>> Stashed changes
   );
 
 }
@@ -197,13 +367,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#111',
-    alignItems: 'center',
-  },  homeContainer: {
-    flex: 1,
+    // alignItems: 'center',
+  },
+  homeContainer: {
+    //flex: 1,
     backgroundColor: '#111',
     alignItems: 'center',
+<<<<<<< Updated upstream
     justifyContent: 'center',
     
+=======
+    //justifyContent: 'center',
+    //paddingBottom: 10,
+>>>>>>> Stashed changes
   },
   logo: {
     width: 305,
@@ -212,10 +388,18 @@ const styles = StyleSheet.create({
   },
   instructions: {
     color: '#888',
+<<<<<<< Updated upstream
     fontSize: 18,
     marginHorizontal: 15,
     
     },
+=======
+ //   fontSize: 18,
+    //marginHorizontal: 15,
+
+  },
+
+>>>>>>> Stashed changes
   button: {
     backgroundColor: 'blue',
     padding: 20,
@@ -234,6 +418,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 200,
     padding: 10,
+<<<<<<< Updated upstream
     backgroundColor: 'white',
   },
   image:{
@@ -244,5 +429,22 @@ const styles = StyleSheet.create({
     padding: 20,
     //flexShrink: 1,
     //flex: 0.5
+=======
+    backgroundColor: 'black',
+    color: 'white',
+    padding: 20,
+    borderBottomColor: "blue"
+  },
+  image: {
+    borderRadius: 14,
+    width: ITEM_WIDTH,
+    height: ITEM_HEIGHT,
+  },
+  contentContainer: {
+    marginTop: 50,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+>>>>>>> Stashed changes
   }
 });
---
+++ Data.js
@@ -0,0 +1,45 @@
+export const data = [
+    {
+        id: '1',
+        homeTeam: 'Sunset Beach FC',
+        awayTeam: 'Alignment FC',
+        matchType: 'Friendly',
+        matchTime: 'Sept 14th, 5:00pm',
+        video_url:
+        'https://images.unsplash.com/photo-1624880357913-a8539238245b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb3RiYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+            userTag: 'ad56gctrasa67',
+
+    },
+
+    {
+        id: '2',
+        homeTeam: "Redeemer's Eagles",
+        awayTeam: 'Bosna',
+        matchType: 'Vancouver League',
+        matchTime: 'Sept 17th, 6:00pm',
+        video_url:
+            'https://images.unsplash.com/photo-1553778263-73a83bab9b0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb3RiYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+            userTag: 'ad56gctrasa67',
+
+    },
+    {
+        id: '3',
+        homeTeam: "Redeemer's Eagles",
+        awayTeam: 'Bosna',
+        matchType: 'Vancouver League',
+        matchTime: 'Sept 17th, 6:00pm',
+        video_url:
+        'https://images.unsplash.com/photo-1431324155629-1a6deb1dec8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGFtYXRldXIlMjBmb290YmFsbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+        userTag: 'ad56gctrasa67',
+    },
+    {
+        id: '4',
+        homeTeam: "Redeemer's Eagles",
+        awayTeam: 'Bosna',
+        matchType: 'Vancouver League',
+        matchTime: 'Sept 17th, 6:00pm',
+        video_url:
+            'https://images.unsplash.com/photo-1505748641491-f7ee2fd6fb4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGNoaWxkcmVuJTIwZm9vdGJhbGx8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60',
+            userTag: 'ad56gctrasa67',
+        },
+];
